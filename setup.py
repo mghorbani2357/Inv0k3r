fbab7b767893a11787ad1a60b86660d3e433d3ac
mghorbani2357 <m.ghorbani2357@gmail.com>
2025-08-15 22:51:33 +0330

    setup file updated

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ __author__ = "Mohsen Ghorbani"
 __email__ = "m.ghorbani2357@gmail.com"
 __copyright__ = "Copyright 2025, Mohsen Ghorbani"
 
-from setuptools import setup, find_packages
-
-
-REQUIREMENTS = filter(None, open('requirements.txt').read().splitlines())
+from setuptools import setup
 
 setup(
+    install_requires=filter(None, open('requirements.txt').read().splitlines())
 )
